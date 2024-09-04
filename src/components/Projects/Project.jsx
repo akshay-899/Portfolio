d7fef9082cadc400113f67ce108d4daa4e77ae00
M Akshay Prabhu <makshayprabhu2003@gmail.com>
2024-09-04 13:53:47 +0530

    project section done

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,9 +1,80 @@
-import React from 'react'
+import React from 'react';
+import './project.css';
+import petbondo from '/assets/petbondo.jpg'
+import uhi from '/assets/uhi.jpg'
+import deskguard from '/assets/deskguard.jpg'
+import jfood from '/assets/jfood.jpg'
+import sudoku from '/assets/sudoku.jpg'
+import examduty from '/assets/examduty.png'
 
 function Project() {
+  const projects = [
+    {
+      id: 1,
+      title: 'Petbondo',
+      description: 'Petbondo is an animal adoption platform.',
+      imageUrl: petbondo,
+      link: 'https://github.com/akshay-899/Petbondo'
+    },
+    {
+      id: 2,
+      title: 'UHI',
+      description: 'An web application for healthcare management.',
+      imageUrl: uhi,
+      link: 'https://github.com/akshay-899/UHI'
+    },
+    {
+      id: 3,
+      title: 'Desk Guard',
+      description: 'A security camera system designed for workspace protection.',
+      imageUrl: deskguard,
+      link: 'https://github.com/akshay-899/Desk_Guard'
+    },
+    {
+      id: 4,
+      title: 'Exam Duty Scheduler',
+      description: 'An web application to schedule duty for the faculty.',
+      imageUrl: examduty,
+      link: 'https://github.com/Mini-Project2024/Exam-Duty-scheduler'
+    },
+    {
+      id: 5,
+      title: 'Jfood',
+      description: 'A food delivery web application',
+      imageUrl: jfood,
+      link: 'https://github.com/akshay-899/Jfood'
+    },
+    {
+      id: 6,
+      title: 'Sudoku',
+      description: 'Sudoku game implementation ',
+      imageUrl: sudoku,
+      link: 'https://github.com/akshay-899/sudoku'
+    },
+  ];
+
   return (
-    <div>Project</div>
-  )
+    <div className="projects-page">
+      <h1 className="projects-heading">Projects</h1>
+        <div className="projects-grid">
+          {projects.map(project => (
+            <a href={project.link} key={project.id} className="project-card" target="_blank" rel="noopener noreferrer">
+              <img 
+                src={project.imageUrl}
+                alt={`${project.title} Thumbnail`} 
+                className="project-image"
+              />
+              <div className="project-content">
+                <h3 className="project-title">{project.title}</h3>
+                <p className="project-description">
+                  {project.description}
+                </p>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+  );
 }
 
-export default Project
+export default Project;
--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,18 +1,18 @@
-import React  from 'react'
-import '../Skill/skill.css'
+import React from "react";
+import "../Skill/skill.css";
 // import reactlogo from '/assets/react.png'
-import django from '/assets/django.svg'
-import express from '/assets/express.svg'
-import mysql from '/assets/mysql.png'
-import mongodb from '/assets/mongo.svg'
-import cpp from '/assets/cpp.svg'
-import c from '/assets/c.svg'
-import java from '/assets/java.svg'
-import python from '/assets/python.svg'
-import git from '/assets/git.svg'
-import github from '/assets/github.svg'
-import vscode from '/assets/vs-code.svg'
-import canva from '/assets/canva.svg'
+import django from "/assets/django.svg";
+import express from "/assets/express.svg";
+import mysql from "/assets/mysql.png";
+import mongodb from "/assets/mongo.svg";
+import cpp from "/assets/cpp.svg";
+import c from "/assets/c.svg";
+import java from "/assets/java.svg";
+import python from "/assets/python.svg";
+import git from "/assets/git.svg";
+import github from "/assets/github.svg";
+import vscode from "/assets/vs-code.svg";
+import canva from "/assets/canva.svg";
 
 function Skill() {
   return (
@@ -20,32 +20,69 @@ function Skill() {
       <div className="skillmain">
         <div className="skill">Skills</div>
         <div className="skillbox">
-        <div className="skill-item">
-          <div className="frameworks-and-library">Web Development</div>
-          <img src={django} className="skill-item-icon" title='Django'></img>
-          <img src={express} className="skill-item-icon" title='Express Js'></img>
-          {/* <img src={reactlogo} className="skill-item-icon"></img> */}
-          <img src={mysql} className="skill-item-icon" title='Mysql'></img>
-          <img src={mongodb} className="skill-item-icon" title='MongoDB'></img>
+          <div className="skill-item">
+            <div className="frameworks-and-library">Web Development</div>
+              <img
+                src={django}
+                className="skill-item-icon"
+                title="Django"
+              ></img>
+              <img
+                src={express}
+                className="skill-item-icon"
+                title="Express Js"
+              ></img>
+              {/* <img src={reactlogo} className="skill-item-icon"></img> */}
+              <img src={mysql} className="skill-item-icon" title="Mysql"></img>
+              <img
+                src={mongodb}
+                className="skill-item-icon"
+                title="MongoDB"
+              ></img>
           </div>
           <div className="skill-item">
-          <div className="programming-languages">Programming Languages</div>
-          <img src={cpp} alt="C++" className="skill-item-icon" title="C++"/>
-          <img src={c} alt="C" className="skill-item-icon" title="C"/>
-          <img src={java} alt="Java" className="skill-item-icon" title="Java"/>
-          <img src={python} alt="Python" className="skill-item-icon" title="Python"/>
+            <div className="programming-languages">Programming Languages</div>
+            <img src={cpp} alt="C++" className="skill-item-icon" title="C++" />
+            <img src={c} alt="C" className="skill-item-icon" title="C" />
+            <img
+              src={java}
+              alt="Java"
+              className="skill-item-icon"
+              title="Java"
+            />
+            <img
+              src={python}
+              alt="Python"
+              className="skill-item-icon"
+              title="Python"
+            />
           </div>
           <div className="skill-item">
-          <div className="tools">Tools</div>
-          <img src={git} alt="Git" className="skill-item-icon" title="Git"/>
-          <img src={github} alt="GitHub" className="skill-item-icon" title="GitHub"/>
-          <img src={vscode} alt="VS Code" className="skill-item-icon" title="Visual Studio Code"/>
-          <img src={canva} alt="Canva" className="skill-item-icon" title="Canva"/>
+            <div className="tools">Tools</div>
+            <img src={git} alt="Git" className="skill-item-icon" title="Git" />
+            <img
+              src={github}
+              alt="GitHub"
+              className="skill-item-icon"
+              title="GitHub"
+            />
+            <img
+              src={vscode}
+              alt="VS Code"
+              className="skill-item-icon"
+              title="Visual Studio Code"
+            />
+            <img
+              src={canva}
+              alt="Canva"
+              className="skill-item-icon"
+              title="Canva"
+            />
           </div>
-          </div>
-          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Skill
+export default Skill;

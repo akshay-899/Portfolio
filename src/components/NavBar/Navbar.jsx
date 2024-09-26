2aaf42ed47ad968ec9ec75dfb04e636ea1068632
M Akshay Prabhu <makshayprabhu2003@gmail.com>
2024-09-26 13:52:52 +0530

    Hamburger menu done

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,20 +1,34 @@
-// src/components/Navbar/Navbar.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
 import logo from '/assets/logo.gif';
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false); 
+  };
+
   return (
     <>
       <nav className="navbar">
-        <img src={logo} alt="logo" className='logo'/>
-        <div className="navcontent">
-          <Link to="/" className="nav-link">Home</Link>
-          <Link to="/about" className="nav-link">About</Link>
-          <Link to="/skills" className="nav-link">Skills</Link>
-          <Link to="/projects" className="nav-link">Projects</Link>
-          <Link to="/contact" className="nav-link">Contact</Link>
+        <img src={logo} alt="logo" className='logo' />
+        <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className={`navcontent ${isOpen ? 'open' : ''}`}>
+          <Link to="/" className="nav-link" onClick={closeMenu}>Home</Link>
+          <Link to="/about" className="nav-link" onClick={closeMenu}>About</Link>
+          <Link to="/skills" className="nav-link" onClick={closeMenu}>Skills</Link>
+          <Link to="/projects" className="nav-link" onClick={closeMenu}>Projects</Link>
+          <Link to="/contact" className="nav-link" onClick={closeMenu}>Contact</Link>
         </div>
       </nav>
     </>
